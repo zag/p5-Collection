2023ba049f07bd1801da546274105ff3f3b7d482
Aliaksandr Zahatski <github.send@zag.ru>
2012-07-31 18:42:18 +0400

    improve index mod

--- lib/Collection/Index.pm
+++ lib/Collection/Index.pm
@@ -20,28 +20,68 @@ use base 'Collection::CrcColl';
 sub put {
     my $self = shift;
     my %recs = @_;
-    my $table_name = $self->_table_name();
-    my $field      = $self->_key_field;
-    my $fields = $self->_fields;
-    my %records =();
-    while ( my ( $k, $v ) = each %recs ) {
-    #clean undeclared fields
-    # TODO:   Collection::ColCrc:    
-    #             unless (exists $fields->{$key}) {
-    #                #skip undeclared fields
-    #              next; }
-    my %dirty_record = ( %$v, $field=> $k ); #add id field for create
-    my $record2save = {};
-    while ( my ($k, $v ) = each %dirty_record ) {
-            next unless exists $fields->{$k};
-            $record2save->{$k} = $v
-    }
-    $records{$k} =  $record2save;
-
-    };
-
-    $self->_store(\%records);
+    $self->_store(\%recs);
 }
+
+sub before_save {
+    my $self                   = shift;
+    my $attr                   = shift;
+    my $fields                 = $self->_fields;
+    my $rec                    = {};
+    my $exists_array_in_values = 0;
+    my $max_array_size         = 1;
+
+    #strip unknown records
+    #for arrays in values calc max size
+    while ( my ( $key, $val ) = each %$attr ) {
+        next unless exists $fields->{$key};
+        $rec->{$key} = $val;
+        if ( ref($val) eq 'ARRAY' ) {
+            ++$exists_array_in_values;
+            my $size = scalar(@$val);
+
+            #set max of arrays
+            $max_array_size = $size if $max_array_size < $size;
+        }
+    }
+
+    #if simple record just save
+    unless ($exists_array_in_values) {  
+        return $self->SUPER::before_save($rec);
+    }
+    else {
+
+        # for record { item1=>1, item2=[1,2]}
+        # make [  {item1=>1, item2=>1}, {item1=>1, item2=>2} ]
+        my @records_for_save;
+
+        #convert values to arrays refs
+        while ( my ( $key, $val ) = each %$rec ) {
+
+            #convert scalar value to array
+            my @values = ref($val) ? @$val : ($val);
+
+            #enlarge to max size
+            if ( my $diff = $max_array_size - scalar(@values) ) {
+                push @values, ( $values[-1] ) x $diff;
+            }
+            $rec->{$key} = \@values;
+
+            #also autovivificate
+            for ( 0 .. $max_array_size - 1 ) {
+                $records_for_save[$_]->{$key} = $values[$_];
+            }
+        }
+        my @res = ();
+        foreach my $record (@records_for_save) {
+            push @res, $self->SUPER::before_save($record);
+        }
+        return \@res
+
+    }
+
+}
+
 
 #no any ActiveRecord
 sub _prepare_record {
